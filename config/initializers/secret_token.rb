6813bb40f3466ef348ffb3045bdf228d74dd6681
Jesus Anderson <ffv.akmr@gmail.com>
2013-05-27 17:57:36 -0600

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppAnderson::Application.config.secret_token = '3c5fe5e355a6191d42a80e5100d28838fde4b138981e4d3eba34dcee5b80c6f5d5bf2b1dad081194ea10506d9459963838545ef609605f86e713382b4a4b193b'
